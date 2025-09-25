79752e6c7b7ab252d01ed9f10da6ac69da564738
Georgi Tasev <georgi.tasev12@gmail.com>
2025-09-25 14:24:58 +0300

    Update on the ListTileItem and created the ComboBoxMenu, added a prefix combo box menu for having the edit and remove notes.

--- app/src/main/java/com/georgitasev/scribble/ui/views/composables/ListTileItem.kt
+++ app/src/main/java/com/georgitasev/scribble/ui/views/composables/ListTileItem.kt
@@ -1,6 +1,8 @@
 package com.georgitasev.scribble.ui.views.composables
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
@@ -8,13 +10,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun ListTileItem(title: String, description: String = "") {
+fun ListTileItem(
+    title: String,
+    description: String = ""
+) {
     Surface(
         shape = AbsoluteRoundedCornerShape(percent = 14),
         color = MaterialTheme.colorScheme.surfaceContainer,
@@ -22,33 +28,32 @@ fun ListTileItem(title: String, description: String = "") {
             .fillMaxWidth()
             .padding(bottom = 20.dp)
     ) {
-        Column {
-            Text(
-                title,
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-                fontSize = 18.sp,
-                fontFamily = FontFamily.Monospace,
-                modifier = Modifier.padding(
-                    vertical = 10.dp,
-                    horizontal = 12.dp
-                )
-            )
-            description.takeIf { it.isNotBlank() }?.let { text ->
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
                 Text(
-                    text,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer.copy(
-                        alpha = 20.0f
-                    ),
-                    fontSize = 12.sp,
+                    title,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    fontSize = 18.sp,
                     fontFamily = FontFamily.Monospace,
-                    maxLines = 2,
-                    softWrap = true,
-                    modifier = Modifier.padding(
-                        vertical = 10.dp,
-                        horizontal = 12.dp
-                    )
+                    modifier = Modifier.padding(vertical = 4.dp)
                 )
+                description.takeIf { it.isNotBlank() }?.let { text ->
+                    Text(
+                        text,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        fontSize = 12.sp,
+                        fontFamily = FontFamily.Monospace,
+                        maxLines = 2,
+                        softWrap = true,
+                        modifier = Modifier.padding(vertical = 4.dp)
+                    )
+                }
             }
+            ComboBoxMenu()
         }
     }
 }
