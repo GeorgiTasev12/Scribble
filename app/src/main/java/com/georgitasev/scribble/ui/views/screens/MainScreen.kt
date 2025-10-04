b38f4730be22faf05a12f1fcd5cfef2cecba0f71
Georgi Tasev <georgi.tasev12@gmail.com>
2025-10-04 14:36:43 +0300

    Created the MainViewModel and established the link on the MainScreen, as well as created the Alert Dialog for the deleting query and updated the List Tile item.

--- app/src/main/java/com/georgitasev/scribble/ui/views/screens/MainScreen.kt
+++ app/src/main/java/com/georgitasev/scribble/ui/views/screens/MainScreen.kt
@@ -1,46 +1,89 @@
 package com.georgitasev.scribble.ui.views.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.georgitasev.scribble.models.Routes
 import com.georgitasev.scribble.ui.views.CoreScreen
 import com.georgitasev.scribble.ui.views.composables.ListTileItem
+import com.georgitasev.scribble.viewmodels.MainViewModel
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import com.georgitasev.scribble.ui.views.composables.AlertDialog
 
 @Composable
 fun MainScreen(
+    viewModel: MainViewModel = viewModel(),
     navController: NavHostController
 ) {
+    val notesList = viewModel.notes.collectAsState().value
+    val openDialog = remember { mutableStateOf(false) }
+
     CoreScreen(
         hasFAB = true,
         onFABClick = {
-            navController.navigate(route = Routes.DETAILS_SCREEN.name)
+            navController.navigate(Routes.DETAILS_SCREEN.name)
         },
-        body = {
-                innerPadding ->
-            LazyColumn(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .padding(
-                        horizontal = 12.dp,
-                        vertical = 10.dp
+        body = { innerPadding ->
+            if (notesList.isEmpty()) {
+                Column(
+                    Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        "Notes are empty, please add some notes",
+                        color = MaterialTheme.colorScheme.onPrimary
                     )
-            ) {
-                items(count = 1) {
-                    ListTileItem(title = "Hello World")
                 }
+            } else {
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .padding(
+                            horizontal = 12.dp,
+                            vertical = 10.dp
+                        )
+                ) {
+                    items(count = notesList.size) { id ->
+                        ListTileItem(
+                            title = notesList[id].title,
+                            description = notesList[id].description,
+                            onEditClick = {
+                                navController.navigate("${Routes.DETAILS_SCREEN.name}/${notesList[id].id}")
+                            },
+                            onDeleteButtonClick = {
+                                openDialog.value = true
+                            }
+                        )
 
-                items(count = 2) {
-                    ListTileItem(title = "Hello World", description = "dhakjshdkjahskjd")
-                }
-
-                items(count = 1) {
-                    ListTileItem(title = "Hello World")
+                        if (openDialog.value) {
+                            AlertDialog(
+                                dialogTitle = "Warning",
+                                dialogContent = "You're about to remove the note from your list. Once removed, it cannot be restored. Are you sure you want to proceed?",
+                                onConfirmation = {
+                                    viewModel.removeNote(id = notesList[id].id)
+                                    openDialog.value = false
+                                },
+                                onDismissRequest = {
+                                    openDialog.value = false
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -51,5 +94,5 @@ fun MainScreen(
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainScreen() {
-    MainScreen(rememberNavController())
+    MainScreen(navController = rememberNavController())
 }
