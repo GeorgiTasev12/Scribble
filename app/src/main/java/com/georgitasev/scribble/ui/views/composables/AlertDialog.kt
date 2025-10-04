057db6c9541362df80746a159dec94c76a604e54
Georgi Tasev <georgi.tasev12@gmail.com>
2025-10-04 15:18:17 +0300

    Update on the Alert Dialog, removed unused imports.

--- app/src/main/java/com/georgitasev/scribble/ui/views/composables/AlertDialog.kt
+++ app/src/main/java/com/georgitasev/scribble/ui/views/composables/AlertDialog.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
