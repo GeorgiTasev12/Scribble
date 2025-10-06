aa7d5ed1334d119b763c4f582039a07d51459ce5
Georgi Tasev <georgi.tasev12@gmail.com>
2025-10-06 14:50:31 +0300

    Update on alert dialog and list tile item, removed unused imports.

--- app/src/main/java/com/georgitasev/scribble/ui/views/composables/AlertDialog.kt
+++ app/src/main/java/com/georgitasev/scribble/ui/views/composables/AlertDialog.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
--- app/src/main/java/com/georgitasev/scribble/ui/views/composables/ListTileItem.kt
+++ app/src/main/java/com/georgitasev/scribble/ui/views/composables/ListTileItem.kt
@@ -11,7 +11,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
