a12ede0e727a14fb83e7c988f2bff8062035be67
Georgi Tasev <georgi.tasev12@gmail.com>
2025-10-12 13:50:16 +0300

    Update on codemagic.yaml and gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,12 +35,12 @@ android {
     signingConfigs {
         create("release") {
             if (System.getenv()["CI"].toBoolean()) { // CI=true is exported by Codemagic
-                storeFile = rootProject.file("codemagic.keystore")
+                storeFile = System.getenv()["CM_KEYSTORE_PATH"]?.let { file(it) }
                 storePassword = System.getenv()["CM_KEYSTORE_PASSWORD"]
                 keyAlias = System.getenv()["CM_KEY_ALIAS"]
                 keyPassword = System.getenv()["CM_KEY_PASSWORD"]
             } else {
-                storeFile = rootProject.file("codemagic.keystore")
+                storeFile = file(keystoreProperties.getProperty("storeFile"))
                 storePassword = keystoreProperties.getProperty("storePassword")
                 keyAlias = keystoreProperties.getProperty("keyAlias")
                 keyPassword = keystoreProperties.getProperty("keyPassword")
