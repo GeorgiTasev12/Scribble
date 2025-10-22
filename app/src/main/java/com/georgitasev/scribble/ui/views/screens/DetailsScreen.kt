7d18ba625e398394c294664e6738bd96afc31b62
Georgi Tasev <georgi.tasev12@gmail.com>
2025-10-22 18:01:45 +0300

    Update on the details screen, checks if the title is empty, fill it to the description, clear the description text field and save/update it blank.

--- app/src/main/java/com/georgitasev/scribble/ui/views/screens/DetailsScreen.kt
+++ app/src/main/java/com/georgitasev/scribble/ui/views/screens/DetailsScreen.kt
@@ -70,9 +70,16 @@ fun DetailsScreen(
         onPopClick = { navController.popBackStack() },
         onSaveNoteClick = {
             noteId?.let { id ->
-                viewModel.updateNote(id, title, description)
+                viewModel.updateNote(
+                    id = id,
+                    title = title.ifEmpty { description },
+                    description = if (title.isEmpty()) "" else description
+                )
             } ?: run {
-                viewModel.createNote(title, description)
+                viewModel.createNote(
+                    title = title.ifEmpty { description },
+                    description = if (title.isEmpty()) "" else description
+                )
             }
             navController.popBackStack()
         },
